f80e52f857933c5bf298dcf16e46f0c86599def0
Ethmane Didi <ousman.didibela@gmail.com>
2023-05-18 14:18:30 +0200

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
-import NavBar from './components/NavBar';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CartProvider from './contexts/CartContext';
+import HomePage from './pages/home';
+import Product from './pages/product';
+import Cart from './pages/cart.jsx';
+import Checkout from './pages/CheckoutSuccess';
+import Contact from './pages/contact';
+import Layout from './components/Layout';
+import CartIcon from './components/CartIcon';
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = product => {
+    setCart([...cart, product]);
+  };
+
   return (
-    <div>
-      <NavBar />
-    </div>
+    <CartProvider value={{ cart, handleAddToCart }}>
+      <Router>
+        <CartIcon cartCount={cart.length} />
+        <Layout>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/product/:id' element={<Product />} />
+            <Route path='/CartPage' element={<Cart />} />
+            <Route path='/checkout' element={<Checkout />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/cart' element={<Cart />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </CartProvider>
   );
 }
 
